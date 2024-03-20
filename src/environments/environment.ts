6175bce194dfbdbc2244d3c52307902c64034a17
Lucas-dev23 <Ldelimacardoso.ll@gmail.com>
2024-03-20 17:14:23 -0300

    feat: desenvolvimento da interface de criação e autenticação de usuários

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const config = {
+
+    /* mapeamento dos endereços do backend */
+    apiUrlClientes : 'http://localhost:8081/api',
+    apiUrlUsuarios : 'http://localhost:8082/api',
+
+    /* mapeamento das variáveis da local storage */
+    authEmail : 'auth-email',
+    authToken : 'auth-token'
+};
